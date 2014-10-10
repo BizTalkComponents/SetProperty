8e6bf73cbba0b00e8678d0dbdb594c43b963d409
robinhultman <robin.hultman@gmail.com>
2014-10-10 11:20:40 +0200

    Implemented SetProperty

---
+++ Src/SetProperty/SetProperty.Component.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace BizTalkComponents.PipelineComponents.SetProperty
+{
+    public partial class SetProperty
+    {
+        public string Name { get { return "SetProperty"; } }
+        public string Version { get { return "1.0"; } }
+        public string Description { get { return "Promotes a value to a specified context property."; } }
+
+        public void GetClassID(out Guid classID)
+        {
+            classID = new Guid("DF2A20CE-F884-469C-B4DC-0ADFF1E51111");
+        }
+
+        public void InitNew()
+        {
+            
+        }
+
+        public IEnumerator Validate(object projectSystem)
+        {
+            return null;
+        }
+
+        public IntPtr Icon { get { return IntPtr.Zero; } }
+    }
+}
--- Src/SetProperty/SetProperty.cs
+++ Src/SetProperty/SetProperty.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BizTalkComponents.Utils.PropertyBagHelpers;
+using Microsoft.BizTalk.Component.Interop;
+using Microsoft.BizTalk.Message.Interop;
+using BizTalkComponents.Utils.ContextPropertyHelpers;
 
-namespace BizTalkComponents.SetProperty
+namespace BizTalkComponents.PipelineComponents.SetProperty
 {
-    public class SetProperty
+    [ComponentCategory(CategoryTypes.CATID_PipelineComponent)]
+    [System.Runtime.InteropServices.Guid("D219F6DA-473A-4E3C-B0C3-24F764A22148")]
+    [ComponentCategory(CategoryTypes.CATID_Any)]
+    public partial class SetProperty : IComponent, IBaseComponent,
+                                        IPersistPropertyBag, IComponentUI
     {
+        private const string PropertyPathPropertyName = "PropertyPath";
+        private const string ValuePropertyName = "Value";
+
+        public string PropertyPath { get; set; }
+        public string Value { get; set; }
+
+        public IBaseMessage Execute(IPipelineContext pContext, IBaseMessage pInMsg)
+        {
+            pInMsg.Context.Promote(new ContextProperty(PropertyPath),Value);
+
+            return pInMsg;
+        }
+
+        public void Load(IPropertyBag propertyBag, int errorLog)
+        {
+            if (string.IsNullOrEmpty(PropertyPath))
+            {
+                PropertyPath = PropertyBagHelper.ToStringOrDefault(PropertyBagHelper.ReadPropertyBag(propertyBag, PropertyPathPropertyName), string.Empty);
+            }
+
+            if (string.IsNullOrEmpty(Value))
+            {
+                Value = PropertyBagHelper.ToStringOrDefault(PropertyBagHelper.ReadPropertyBag(propertyBag, ValuePropertyName), string.Empty);
+            }   
+        }
+
+        public void Save(IPropertyBag propertyBag, bool clearDirty, bool saveAllProperties)
+        {
+            PropertyBagHelper.WritePropertyBag(propertyBag, PropertyPathPropertyName, PropertyPath);
+            PropertyBagHelper.WritePropertyBag(propertyBag, ValuePropertyName, Value);
+        }
     }
 }
