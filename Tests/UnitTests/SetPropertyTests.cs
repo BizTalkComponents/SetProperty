2e65da942059daead8c83cc1a7757c09948b588f
robinhultman <robin.hultman@gmail.com>
2014-10-10 11:44:16 +0200

    Added unit tests

---
+++ Tests/UnitTests/SetPropertyTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Winterdom.BizTalk.PipelineTesting;
+
+namespace BizTalkComponents.SetProperty.Tests.UnitTests
+{
+    [TestClass]
+    public class SetPropertyTests
+    {
+        [TestMethod]
+        public void TestSetProperty()
+        {
+            var pipeline = PipelineFactory.CreateEmptyReceivePipeline();
+            var component = new PipelineComponents.SetProperty.SetProperty
+            {
+                PropertyPath = "http://tempuri.org#MyProp",
+                Value = "Test"
+            };
+
+            pipeline.AddComponent(component, PipelineStage.Decode);
+
+            var message = MessageHelper.Create("<test></test>");
+
+
+            Assert.IsNull(message.Context.Read("MyProp", "http://tempuri.org"));
+
+            var output = pipeline.Execute(message);
+
+            Assert.AreEqual(1, output.Count);
+
+            Assert.IsTrue(output[0].Context.IsPromoted("MyProp", "http://tempuri.org"));
+        }
+    }
+}
