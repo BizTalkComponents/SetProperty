5c2ba1497744c2efb9f680450f2bd072dd6de8a7
robinhultman <robin.hultman@gmail.com>
2015-04-23 13:37:10 +0200

    Added new unit tests to test switch between write and promote

--- Tests/UnitTests/SetPropertyTests.cs
+++ Tests/UnitTests/SetPropertyTests.cs
@@ -30,5 +30,55 @@ namespace BizTalkComponents.PipelineComponents.SetProperty.Tests.UnitTests
 
             Assert.IsTrue(output[0].Context.IsPromoted("MyProp", "http://tempuri.org"));
         }
+
+
+        [TestMethod]
+        public void TestSetPropertyWrite()
+        {
+            var pipeline = PipelineFactory.CreateEmptyReceivePipeline();
+            var component = new SetProperty
+            {
+                PropertyPath = "http://tempuri.org#MyProp",
+                Value = "Test",
+                PromoteProperty = false
+            };
+
+            pipeline.AddComponent(component, PipelineStage.Decode);
+
+            var message = MessageHelper.Create("<test></test>");
+
+
+            Assert.IsNull(message.Context.Read("MyProp", "http://tempuri.org"));
+
+            var output = pipeline.Execute(message);
+
+            Assert.AreEqual(1, output.Count);
+
+            Assert.IsFalse(output[0].Context.IsPromoted("MyProp", "http://tempuri.org"));
+        }
+
+        [TestMethod]
+        public void TestSetPropertyPromotePropertyNotSet()
+        {
+            var pipeline = PipelineFactory.CreateEmptyReceivePipeline();
+            var component = new SetProperty
+            {
+                PropertyPath = "http://tempuri.org#MyProp",
+                Value = "Test"
+            };
+
+            pipeline.AddComponent(component, PipelineStage.Decode);
+
+            var message = MessageHelper.Create("<test></test>");
+
+
+            Assert.IsNull(message.Context.Read("MyProp", "http://tempuri.org"));
+
+            var output = pipeline.Execute(message);
+
+            Assert.AreEqual(1, output.Count);
+
+            Assert.IsFalse(output[0].Context.IsPromoted("MyProp", "http://tempuri.org"));
+        }
     }
 }
